28716c8a34ad3102a5a9c30b8410fdbf70fa4b7a
dream-topdev <williamsmoisha@gmail.com>
2023-08-17 17:58:19 -0400

    add

---
+++ index.js
@@ -0,0 +1,14 @@
+// Require dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const cors = require('cors')
+const path = require('path');
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/animation', express.static(path.join(__dirname, 'animation')))
+// Handle files
+app.listen(3000, '127.0.0.1', () => {
+    console.log('Server started on port 3000');
+});
