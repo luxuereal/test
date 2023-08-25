1be2462075004614163c2f52de5681fb9651d1f0
Deverabit <131601029+bravedevrabbit@users.noreply.github.com>
2023-08-24 21:40:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/animation', express.static(path.join(__dirname, 'animation')))
-// Handle files
+// Handle files automatically
 app.listen(3000, '127.0.0.1', () => {
     console.log('Server started on port 3000');
 });
